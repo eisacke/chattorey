9d2051ba11ff0680af2c76b342a83bc55f29508f
Emily Isacke <emily@isacke.com>
2019-02-06 11:29:15 +0000

    Adding favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,20 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-stylus`
+    `gatsby-plugin-stylus`,
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Chattorey",
+        short_name: "Chattorey",
+        start_url: "/",
+        background_color: "#FFFFFF",
+        theme_color: "#FFFFFF",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png" // This path is relative to the root of the site.
+      }
+    }
   ]
 };
