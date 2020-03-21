1d0447f6a9c7f8ee00199f9317b88a4e8154b137
eisacke <emily@isacke.com>
2020-03-21 17:07:44 +0000

    Add COVID-19 closure banner

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -37,40 +37,49 @@ class Header extends Component {
 
   render() {
     return (
-      <div className={headerStyles.container}>
-        <div className="grid">
-          <div className="row">
-            <div className="c5 lrg-c12 lrg-text-center lrg-half-push-bottom">
-              <Link to="/" className={headerStyles.logo}>
-                <h1 className="heading">{this.props.siteTitle}</h1>
-              </Link>
+      <>
+        <div className={headerStyles.banner}>
+          <div className="grid">
+            <div className="row">
+              <div className="c12">
+                <p className={headerStyles.bannerTitle}>Temporary Closure</p>
+                Unfortunately due to the COVID-19 pandemic and in accordance
+                with the latest social distancing guidelines we have had to
+                close until further notice. We look forward to seeing you when
+                we reopen.
+              </div>
             </div>
-            <div className={headerStyles.navLinksWrapper + " c7 lrg-c12"}>
-              <ul className={headerStyles.navLinks}>
-                {links.map((link, index) => (
-                  <li key={index} className={headerStyles.navLink}>
-                    <Link
-                      to={link.url}
-                      activeClassName={headerStyles.navLinkActive}
-                    >
-                      {link.text}
-                    </Link>
-                  </li>
-                ))}
-                <li className={headerStyles.navLink + " hide med-show"}>
-                  <button onClick={this.scrollToFooter}>Contact</button>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            Temporary Closure - Unfortunately due to the COVID-19 pandemic and
-            in accordance with the latest social distancing guidelines we have
-            had to close until further notice. We look forward to seeing you
-            when we reopen.
           </div>
         </div>
-      </div>
+        <div className={headerStyles.container}>
+          <div className="grid">
+            <div className="row">
+              <div className="c5 lrg-c12 lrg-text-center lrg-half-push-bottom">
+                <Link to="/" className={headerStyles.logo}>
+                  <h1 className="heading">{this.props.siteTitle}</h1>
+                </Link>
+              </div>
+              <div className={headerStyles.navLinksWrapper + " c7 lrg-c12"}>
+                <ul className={headerStyles.navLinks}>
+                  {links.map((link, index) => (
+                    <li key={index} className={headerStyles.navLink}>
+                      <Link
+                        to={link.url}
+                        activeClassName={headerStyles.navLinkActive}
+                      >
+                        {link.text}
+                      </Link>
+                    </li>
+                  ))}
+                  <li className={headerStyles.navLink + " hide med-show"}>
+                    <button onClick={this.scrollToFooter}>Contact</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
