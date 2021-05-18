e72e4ebbefaf30ce9cff080e90487cd577a8c264
eisacke <emily@isacke.com>
2021-05-18 10:44:03 +0100

    chore: Remove COVID banner

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -36,56 +36,34 @@ const Header = ({ siteTitle }) => {
   };
 
   return (
-    <>
-      <div className={headerStyles.banner}>
-        <div className="grid">
-          <div className="row">
-            <div className="c12">
-              <p className={headerStyles.bannerTitle}>
-                You can still enjoy our food at home!
-              </p>
-              <p>
-                Due to the current COVID-19 restrictions, we are limiting our
-                opening hours to Friday, Saturday and Sunday. We are offering a
-                takeaway service only.
-              </p>
-              <p>
-                Please call us on <a href="tel:01474322123">01474 322123</a> to
-                order.
-              </p>
-            </div>
+    <div className={headerStyles.container}>
+      <div className="grid">
+        <div className="row">
+          <div className="c5 lrg-c12 lrg-text-center lrg-half-push-bottom">
+            <Link to="/" className={headerStyles.logo}>
+              <h1 className="heading">{siteTitle}</h1>
+            </Link>
           </div>
-        </div>
-      </div>
-      <div className={headerStyles.container}>
-        <div className="grid">
-          <div className="row">
-            <div className="c5 lrg-c12 lrg-text-center lrg-half-push-bottom">
-              <Link to="/" className={headerStyles.logo}>
-                <h1 className="heading">{siteTitle}</h1>
-              </Link>
-            </div>
-            <div className={headerStyles.navLinksWrapper + " c7 lrg-c12"}>
-              <ul className={headerStyles.navLinks}>
-                {links.map((link, index) => (
-                  <li key={index} className={headerStyles.navLink}>
-                    <Link
-                      to={link.url}
-                      activeClassName={headerStyles.navLinkActive}
-                    >
-                      {link.text}
-                    </Link>
-                  </li>
-                ))}
-                <li className={headerStyles.navLink + " hide med-show"}>
-                  <button onClick={scrollToFooter}>Contact</button>
+          <div className={headerStyles.navLinksWrapper + " c7 lrg-c12"}>
+            <ul className={headerStyles.navLinks}>
+              {links.map((link, index) => (
+                <li key={index} className={headerStyles.navLink}>
+                  <Link
+                    to={link.url}
+                    activeClassName={headerStyles.navLinkActive}
+                  >
+                    {link.text}
+                  </Link>
                 </li>
-              </ul>
-            </div>
+              ))}
+              <li className={headerStyles.navLink + " hide med-show"}>
+                <button onClick={scrollToFooter}>Contact</button>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
